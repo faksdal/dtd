9ad6bb2f88aa26a4d0420e91a7519e10ccf10efb
faksdal <jon@jonleithe.no>
2022-10-29 21:17:29 +0200

    restructuring into classes

--- Makefile
+++ Makefile
@@ -1,13 +1,16 @@
  
 CC=g++
 CFLAGS=-I. -I./source
-OBJ = obj/dtd.o
+OBJ = obj/dtd.o obj/degtodec.o
 
 all: $(OBJ)
 	$(CC) $(OBJ) -o dtd
 
 obj/dtd.o: dtd.cpp
 	$(CC) $(CFLAGS) -c -o obj/dtd.o dtd.cpp
+
+obj/degtodec.o: source/degtodec.cpp
+	$(CC) $(CFLAGS) -c -o obj/degtodec.o source/degtodec.cpp
 
 #obj/JulianDay.o : source/JulianDay.h source/JulianDay.cpp
 #	$(CC) $(CFLAGS) -c -o obj/JulianDay.o source/JulianDay.cpp
---
+++ source/degtodec.cpp
@@ -0,0 +1,31 @@
+/*
+ * degtodec.cpp
+ *
+ *  Created on: 28 Oct 2022
+ *      Author: jole
+ *      
+ *	source code for convert deg/min/sec to decimaldegrees      
+ *      
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "source/degtodec.h"
+
+
+dtd::dtd(char *input, mode m)
+{
+	float	retVal;
+	
+	switch(m){
+		case degToDec:	deg = atoi(input);
+						while(*input++ != '.');
+						min = atoi(input);
+						while(*input++ != '.');
+						sec = atoi(input);
+						decDegrees = deg + min/60.0 + sec/3600.0;
+						break;
+		case decToDeg:	break;
+		default:		break;
+	}
+}
